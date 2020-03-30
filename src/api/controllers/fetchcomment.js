a6814fe6c0779ffd3b9efb51f963b009538883fe
harshwalia36 <43993467+harshwalia36@users.noreply.github.com>
2020-03-31 00:11:55 +0530

    correction in fetchcomment.js

--- src/api/controllers/fetchcomment.js
+++ src/api/controllers/fetchcomment.js
@@ -1,5 +1,5 @@
 const {ObjectId}=require('mongodb');
 module.exports= async (req,res,db) =>{
     const comments=await db.collection('comments').find({PostID:req.params.postID}).project({_id:0,comment:1}).toArray();
-    res.send(comments.comment);
+    res.send(comments);
 }
