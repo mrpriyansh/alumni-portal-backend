c2c742a96b2ecd205e2b7ba07e4e8f33aaa5f72e
Gaharana <mr.priyanshgaharana@gmail.com>
2020-05-31 21:40:00 +0530

    remvooved user details from comments and post collections

--- src/api/controllers/fetchPosts.js
+++ src/api/controllers/fetchPosts.js
@@ -1,22 +1,38 @@
-const { ObjectId } = require('mongodb');
-
 module.exports = async (req, res, db) => {
   // eslint-disable-next-line radix
   const offset = parseInt(req.query.offset || 0);
   const pageSize = 5;
-  const { id } = req.params;
   const { type } = req.query;
-  if (id === 'ALL') {
-    // eslint-disable-next-line prettier/prettier
-    const posts = await db.collection('posts').find({type}).skip(offset).limit(pageSize).sort([['_id',-1]]).toArray();
-    const size = await db.collection('posts').countDocuments({ type });
-    const count = size === offset + posts.length ? null : offset + posts.length;
-    res.status(200).json({ offset: count, posts });
-  } else {
-    // eslint-disable-next-line prettier/prettier
-    const posts = await db.collection('posts').find({ID: ObjectId(id), type}).skip(offset).limit(pageSize).sort([['_id',-1]]).toArray();
-    const size = await db.collection('posts').countDocuments({ type, id });
-    const count = size === offset + posts.length ? null : offset + posts.length;
-    res.status(200).json({ offset: count, posts });
-  }
+  const projection = {
+    _id: 1,
+    userId: 1,
+    text: 1,
+    type: 1,
+    url: 1,
+    fileUrls: 1,
+    timestamp: 1,
+    name: 1,
+    profilePicUrl: 1,
+    designation: 1,
+    company: 1,
+  };
+  const posts = await db
+    .collection('posts')
+    .aggregate([
+      { $lookup: { from: 'users', localField: 'userId', foreignField: '_id', as: 'userDetails' } },
+      {
+        $replaceRoot: {
+          newRoot: { $mergeObjects: [{ $arrayElemAt: ['$userDetails', 0] }, '$$ROOT'] },
+        },
+      },
+      { $match: { type } },
+      { $project: projection },
+      { $sort: { _id: -1 } },
+      { $skip: offset },
+      { $limit: pageSize },
+    ])
+    .toArray();
+  const size = await db.collection('posts').countDocuments({ type });
+  const count = size === offset + posts.length ? null : offset + posts.length;
+  res.status(200).json({ offset: count, posts });
 };
--- src/api/controllers/fetchcomment.js
+++ src/api/controllers/fetchcomment.js
@@ -1,16 +1,38 @@
+const { ObjectID } = require('mongodb');
+
 module.exports = async (req, res, db) => {
   // eslint-disable-next-line radix
   const offset = parseInt(req.query.offset || 0);
   const pageSize = Math.max(1, offset);
+  const projection = {
+    _id: 1,
+    commentText: 1,
+    timestamp: 1,
+    userId: 1,
+    name: 1,
+    profilePicUrl: 1,
+  };
 
   const comments = await db
     .collection('comments')
-    .find({ postId: req.params.postId })
-    .skip(offset)
-    .limit(pageSize)
-    .sort([['_id', -1]])
+    .aggregate([
+      { $lookup: { from: 'users', localField: 'userId', foreignField: '_id', as: 'userDetails' } },
+      {
+        $replaceRoot: {
+          newRoot: { $mergeObjects: [{ $arrayElemAt: ['$userDetails', 0] }, '$$ROOT'] },
+        },
+      },
+      { $match: { postId: ObjectID(req.params.postId) } },
+      { $project: projection },
+      { $sort: { _id: -1 } },
+      { $skip: offset },
+      { $limit: pageSize },
+    ])
     .toArray();
-  const size = await db.collection('comments').countDocuments({ postId: req.params.postId });
+  const size = await db
+    .collection('comments')
+    .countDocuments({ postId: ObjectID(req.params.postId) });
   const count = size === offset + comments.length ? null : offset + comments.length;
+  // res.status(200).json(posts);
   res.status(200).json({ offset: count, comments });
 };
